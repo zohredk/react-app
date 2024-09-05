dc57d74e6c91520c12c2dc1e8eea5ccbf5a34997
zohredk <zohrehdk2000@gmail.com>
2024-09-06 08:54:38 +1000

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App.js";
 
-const element = React.createElement(
-  "div",
-  { id: "main", className: "container" },
-  "Hello world!"
-);
-
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
